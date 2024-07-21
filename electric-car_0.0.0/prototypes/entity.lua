46f12c4d19c3ab050caa9a25e852903b042eead0
Buildasaurus <79661189+Buildasaurus@users.noreply.github.com>
2024-07-21 16:41:26 +0200

    Fix acceleration issue
    The problem is if both friction and friction_force are defined, then friction is used, which is predefined by the car.

--- electric-car_0.0.0/prototypes/entity.lua
+++ electric-car_0.0.0/prototypes/entity.lua
@@ -1,16 +1,14 @@
 
-
-
 local electricCarEntity = table.deepcopy(data.raw["car"]["car"]) -- Copy car entity
 electricCarEntity.name = "electric-car-entity"
 electricCarEntity.minable = { result = "electric-car", mining_time = 0.3 }
 electricCarEntity.effectivity = 1
-electricCarEntity.consumption = "100KW"
+electricCarEntity.consumption = "1000KW"
 electricCarEntity.rotation_speed = 0.01
 electricCarEntity.braking_power = "500kW"
 
 electricCarEntity.weight = 500
-electricCarEntity.friction_force = 0.01
+electricCarEntity.friction = 0.01
 
 electricCarEntity.burner = {
     fuel_category = "electrical",

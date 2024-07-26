7747c05f397aa4a92b19932da522b6d66ff58052
Buildasaurus <79661189+Buildasaurus@users.noreply.github.com>
2024-07-26 14:34:26 +0200

    Add electric car temporary image

--- electric-cars_0.0.0/prototypes/entity.lua
+++ electric-cars_0.0.0/prototypes/entity.lua
@@ -16,19 +16,85 @@ data:extend { electricRacerEntity }
 
 
 -- Electrical rover
-local electricRacerEntity = table.deepcopy(data.raw["car"]["car"]) -- Copy car entity
-electricRacerEntity.name = "electric-rover-entity"
-electricRacerEntity.minable = { result = "electric-rover", mining_time = 0.3 }
-electricRacerEntity.consumption = "200kW"
-electricRacerEntity.braking_power = "100kW"
-electricRacerEntity.weight = 700 * 1.8
-electricRacerEntity.friction = (2e-3) * 2
-electricRacerEntity.burner = {
+local electricRoverEntity = table.deepcopy(data.raw["car"]["car"]) -- Copy car entity
+electricRoverEntity.name = "electric-rover-entity"
+electricRoverEntity.minable = { result = "electric-rover", mining_time = 0.3 }
+electricRoverEntity.consumption = "200kW"
+electricRoverEntity.braking_power = "100kW"
+electricRoverEntity.weight = 700 * 1.8
+electricRoverEntity.friction = (2e-3) * 2
+electricRoverEntity.burner = {
     fuel_category = "electrical",
     effectivity = 1,
     fuel_inventory_size = 1,
 }
-data:extend { electricRacerEntity }
+electricRoverEntity.animation = {
+    animation_speed = 8,
+    direction_count = 1,
+    frame_count = 1,
+    height = 170,
+    max_advance = 0.2,
+    priority = "low",
+    shift = {
+        0,
+        -0.1875
+    },
+    scale = 0.5,
+    stripes = {
+        {
+            filename = "__electric-cars__/graphics/electric-racer.png",
+            height_in_frames = 1,
+            width_in_frames = 1
+        }
+    },
+    width = 200
+}
+electricRoverEntity.light = {
+    {
+        type = "oriented",
+        minimum_darkness = 0.3,
+        picture = {
+            filename = "__core__/graphics/light-cone.png",
+            priority = "extra-high",
+            flags = { "light" },
+            width = 200,
+            height = 200,
+            scale = 2
+        },
+        shift = { -0.6, -14 },
+        size = 1,
+        intensity = 0.6,
+        color = { r = 0.92, g = 0.77, b = 0.3 }
+    },
+
+}
+
+electricRoverEntity.light_animation = {
+    filename = "__base__/graphics/entity/car/car-light.png",
+    blend_mode = "additive",
+    draw_as_glow = true,
+    width = 102,
+    height = 84,
+    line_length = 8,
+    direction_count = 64,
+    shift = { 0.0625, -0.15625 },
+    priority = "low",
+    repeat_count = 1,
+    hr_version = {
+        filename = "__base__/graphics/entity/car/hr-car-light.png",
+        blend_mode = "additive",
+        draw_as_glow = true,
+        width = 206,
+        height = 162,
+        line_length = 8,
+        direction_count = 64,
+        shift = { 0.03125, -0.09375 },
+        priority = "low",
+        repeat_count = 1,
+        scale = 0.5
+    }
+}
+data:extend { electricRoverEntity }
 
 
 
@@ -95,7 +161,7 @@ chargingStationEntity.icons = {
         tint = { r = 0.5, g = 0.5, b = 1.0, a = 0.75 }
     },
 }
-chargingStationEntity.flags = {"placeable-neutral", "placeable-player", "player-creation"}
+chargingStationEntity.flags = { "placeable-neutral", "placeable-player", "player-creation" }
 chargingStationEntity.radius_visualisation_specification = {
     sprite = {
         filename = "__electric-cars__/graphics/shadow-circle.png",
